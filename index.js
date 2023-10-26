f4e0dac16f38b9a8b5bb334ea11680d99e4e1bf5
arzumanov2004 <sd8cxz70x@code.edu.az>
2023-10-26 17:15:46 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// //   1-ci sual 
+
+
+// // function counTRY (x) {
+// //   console.log(array.find(item => item.id == x).shipAddress.country);
+
+// // }
+// // counTRY(11063)
+
+
+// // 2 ci sual 
+
+
+
+// // function myTask(x) {
+  
+// // console.log(array.find(item => item.shipAddress.country == x).shipAddress);
+  
+// // }
+
+// // myTask("Ireland")
+
+
+// // 3 -cu sual
+
+
+
+// //  function myTask(x){
+// //      let myArray = array.find(item => item.id == x)
+// //      console.log(myArray.shipName,myArray.shipAddress.country,myArray.shipAddress.city)
+// //  }
+
+// // myTask(11063)
+
+
+// // 4-cu sual
+
+
+// // function myTask(x) {
+  
+// //      let myArray = array.find(item => item.id == x)
+// //      console.log(myArray.details.length);
+
+// // }
+// // myTask(11063)
+
+
+
+// // 5-ci sual
+
+
+// // function myTask(x) {
+// //   let myArray = array.find(item => item.id == x)
+// //   let qazanc = 0
+// //   myArray.details.forEach(item =>{
+// //     qazanc += (1 - item.discount) * (item.unitPrice) * (item.quantity)
+// //   })
+// //   console.log(qazanc);
+// // }
+
+// // myTask(11063)
